9872093955699047dd036702bbb6881d52c8d874
Ross Mackay <ross@mcky.me>
2013-11-11 00:42:08 +0000

    Added new rails 3.2.13 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flat::Application.config.secret_token = 'bf1712bb8f4a25a1ec69d8ca12dc2b1ce8b5cc9d49c8ed28ffeaefc17dbbad42cf2172f8c0a023b5a3af5148b7a8137aa0225ebbafaf938d9f8f4d913e050ea3'
