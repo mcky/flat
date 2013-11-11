a07238f218977241eb63aab601ded7f4217d4d2e
Ross Mackay <ross@mcky.me>
2013-11-11 03:30:48 +0000

    Added bs tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
   
+  validates :name, presence: true,
+                    uniqueness: true,
+                    format: {
+                      with: /\A[a-zA-Z\-\_]+\Z/,
+                      message: "must be formatted correctly."
+                    }
+  
   has_many :expenses
   
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter a name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:name].empty?
+  end
+  
+  test "a user should have a unique name" do 
+    user = User.new
+    user.name = users(:ross).name
+    assert !user.save
+    assert !user.errors[:name].empty?
+  end
+  
+  test "a user should have a name without spaces" do
+    user = User.new
+    user.name = "My name with spaces"
+    assert !user.save
+    assert !user.errors[:name].empty?
+    assert user.errors[:name].include?("must be formatteded correctly")
+  end
 end
